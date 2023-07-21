4cb74d1811e9554fb22895b5797bffa4a4a15e66
sospeter123 <simonnganga187@gmail.com>
2023-07-21 06:07:23 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
-{
+	{
 	char letter;
-
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
-}          
+	}
