717e8a712912bad0a97e0dff829f986374718f3b
sospeter123 <simonnganga187@gmail.com>
2023-08-02 02:46:17 -0700

    my project

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _my_puts_recursion(char *s);
+int _my_putchar(char c);
+void _my_print_rev_recursion(char *s);
+int _my_strlen_recursion(char *s);
+int my_factorial(int n);
+int _my_pow_recursion(int x, int y);
+int _my_sqrt_recursion(int n);
+int my_is_prime_number(int n);
+int my_is_palindrome(char *s);
+
+#endif
